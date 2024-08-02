80fbca1fb59a8226212794e4f34bd55994dbb49b
Miguel Soto <miguelsr.1084@gmail.com>
2024-08-01 23:54:41 -0500

    FEAT: add log

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import express from 'express'
 import cors from 'cors'
+import morgan from 'morgan'
 
 import InitRouter from './infrastructure/handler/router'
+import dotenv from 'dotenv'
 
-const PORT = 3000
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
 
 const app = express()
 
-app.use(cors())
+app.use(morgan('combined'))
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || '*',
+    methods: (process.env.CORS_METHODS || 'GET,POST').split(','),
+  })
+)
 app.use(express.json())
 
 InitRouter({ App: app })
